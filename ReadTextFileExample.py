ab86ad95395c087d56f3292915b9a1d122e7069d
arunkpe <arunkpe@yahoo.com>
2016-12-23 20:01:33 -0800

    Delete ReadTextFileExample.py

--- ReadTextFileExample.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-rateFile      = open("C:\\Documents and Settings\\nbkm79e\\Desktop\\PythonSamples\\PrimeRate.txt","r")
-dateRate      = [line.split() for line in rateFile]
-rateFile.close()
-print(float(dateRate[1][1])+2.25)
