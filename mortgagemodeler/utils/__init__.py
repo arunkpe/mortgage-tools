902a6d279cd710539f31d1a41ad4a24861ca5d2c
arunkpe <arunkpe@yahoo.com>
2025-06-10 00:51:20 -0700

    feat: major release with ARM, recast, refinance, and analytics (v0.4.0)
    - Add ARM (Adjustable Rate Mortgage) support
    - Implement loan recast and refinance functionality
    - Enhance CLI with full analytics capabilities
    - Add curtailments utility for payment modifications
    - Add date utility functions
    - Update core amortizer and loan modules
    - Improve project packaging and installation

--- mortgagemodeler/utils/__init__.py
+++ mortgagemodeler/utils/__init__.py
@@ -7,8 +7,11 @@ from .plot_amortization import plot_amortization_curve
 from .exta_payment_compare import compare_scenarios
 from .plotting import plot_amortization
 from .cost_savings import cost_until_exit
+from .date import end_of_month
+from .curtailments import parse_curtailments
 
 __all__ =["calculate_interest","RateReader", "FredFetcher", \
            "breakeven_analysis","effective_apr","plot_amortization_curve",\
-           "compare_scenarios", "plot_amortization", "cost_until_exit"
+           "compare_scenarios", "plot_amortization", "cost_until_exit", \
+           "end_of_month", "parse_curtailments"
             ]
